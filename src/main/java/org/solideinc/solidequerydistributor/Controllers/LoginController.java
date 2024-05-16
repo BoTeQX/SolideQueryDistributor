7c2a1fbbe6abd21618ff9e39fa2d406c494f9526
SK™ <sohaib1411@hotmail.com>
2024-05-16 21:50:53 +0200

    Created scene/design for login screen
    refactored the main file.

---
+++ src/main/java/org/solideinc/solidequerydistributor/Controllers/LoginController.java
@@ -0,0 +1,9 @@
+package org.solideinc.solidequerydistributor.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class LoginController {
+    @FXML
+    private Pane RootLayout;
+}
--- src/main/java/org/solideinc/solidequerydistributor/Main.java
+++ src/main/java/org/solideinc/solidequerydistributor/Main.java
@@ -3,6 +3,7 @@ package org.solideinc.solidequerydistributor;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +11,19 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setTitle("Solide™ Query Distributor - Login");
         stage.setScene(scene);
         stage.show();
+        stage.setResizable(false);
+        stage.getIcons().add(new Image("/logo.png"));
+
+
+
+
     }
 
     public static void main(String[] args) {
