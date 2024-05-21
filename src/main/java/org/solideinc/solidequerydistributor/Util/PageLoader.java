f5a5307f4ec079a71e84503e5262e6236eb9899a
SK™ <sohaib1411@hotmail.com>
2024-05-21 13:06:46 +0200

    Created a class to load scenes

---
+++ src/main/java/org/solideinc/solidequerydistributor/Util/PageLoader.java
@@ -0,0 +1,57 @@
+package org.solideinc.solidequerydistributor.Util;
+
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.solideinc.solidequerydistributor.Main;
+
+import java.io.IOException;
+
+public class PageLoader {
+
+    public static final String STYLESHEET_PATH = "/org/solideinc/solidequerydistributor/style.css";
+    private static Pane rootLayout;
+    private static Stage stage;
+
+    public static void setRootLayout(Pane rootLayout) {
+        PageLoader.rootLayout = rootLayout;
+    }
+
+    public static void setStage(Stage stage) {
+        PageLoader.stage = stage;
+    }
+
+    public static void startApplication() {
+            Stage primaryStage = new Stage();
+            setStage(primaryStage);
+            setRootLayout(new Pane());
+            loadLoginPage();
+    }
+
+    private static void loadPage(String fxmlFile, String title) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlFile));
+            rootLayout = fxmlLoader.load();
+            Scene scene = new Scene(rootLayout);
+            scene.getStylesheets().add(Main.class.getResource(STYLESHEET_PATH).toExternalForm());
+            stage.setScene(scene);
+            stage.setTitle(title);
+            stage.show();
+            stage.setResizable(false);
+            stage.getIcons().add(new Image("/logo.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void loadLoginPage() {
+        loadPage("Login.fxml", "Solide™ Query Distributor - Login");
+    }
+
+    public static void loadMainPage() {
+        loadPage("Main.fxml", "Solide™ Query Distributor - Main");
+    }
+}
