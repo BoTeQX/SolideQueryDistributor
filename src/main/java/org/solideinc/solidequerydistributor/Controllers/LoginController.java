5e49b2894ebda8f5fcfeb5034f77c001cfefdf94
Drakarta <drakarta3@gmail.com>
2024-05-24 14:56:56 +0200

    Json storage for Users

--- src/main/java/org/solideinc/solidequerydistributor/Controllers/LoginController.java
+++ src/main/java/org/solideinc/solidequerydistributor/Controllers/LoginController.java
@@ -9,6 +9,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import org.solideinc.solidequerydistributor.Classes.User;
 
+import java.io.IOException;
+
 
 public class LoginController {
     @FXML
@@ -24,11 +26,15 @@ public class LoginController {
     private TextField loginPasswordPasswordField;
 
     @FXML
-    private void initialize() {
-        String password = "test";
-        String hashedPassword = BCrypt.withDefaults().hashToString(12, password.toCharArray());
-        UserController.createUser("test@test.nl", "test", hashedPassword, "nl");
-        loginButton.setOnAction(event -> login());
+    private void initialize() throws IOException {
+        UserController.createUser("admin@admin.nl", "admin", "admin", "nl");
+        loginButton.setOnAction(event -> {
+            try {
+                login();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     private String getUsername() {
@@ -80,7 +86,7 @@ public class LoginController {
         return false;
     }
 
-    private void validateLoginCredentials (String username, String password) {
+    private void validateLoginCredentials (String username, String password) throws IOException {
         if (UserController.checkUser(username, password)) {
             redirectUser();
         } else {
@@ -94,7 +100,7 @@ public class LoginController {
         System.out.println("Redirecting user to the main screen");
     }
 
-    private void login() {
+    private void login() throws IOException {
         String username = getUsername();
         String password = getPassword();
 
--- src/main/java/org/solideinc/solidequerydistributor/Util/JsonHandler.java
+++ src/main/java/org/solideinc/solidequerydistributor/Util/JsonHandler.java
@@ -1,24 +1,19 @@
 package org.solideinc.solidequerydistributor.Util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.io.File;
 import java.io.IOException;
 
 public class JsonHandler {
-
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    static {
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    public static <T> T readJson(TypeReference<T> typeReference, String file) throws IOException {
+        return objectMapper.readValue(new File("data/" + file), typeReference);
     }
 
-    public static void writeJson(Object data, String file) throws IOException {
-        objectMapper.writeValue(new File("data/" + file), data);
-    }
-
-    public static <T> T readJson(Class<T> valueType, String file) throws IOException {
-        return objectMapper.readValue(new File("data/" + file), valueType);
+    public static void writeJson(Object obj, String file) throws IOException {
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File("data/" + file), obj);
     }
 }
