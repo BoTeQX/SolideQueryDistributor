178436d2f4ab6dfa5401932191017de2b9d59da8
SK™ <sohaib1411@hotmail.com>
2024-06-06 19:39:28 +0200

    Further extended the Template Pattern
    Added an abstract class with a final loadPage method that can be used to load pages. in the pageloader for all future pages. also added more robust error logging for the method

--- src/main/java/org/solideinc/solidequerydistributor/Util/PageLoader.java
+++ src/main/java/org/solideinc/solidequerydistributor/Util/PageLoader.java
@@ -1,62 +1,25 @@
 package org.solideinc.solidequerydistributor.Util;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import org.solideinc.solidequerydistributor.Interfaces.PageLoaderInterface;
-import org.solideinc.solidequerydistributor.Main;
+import org.solideinc.solidequerydistributor.Abstract.AbstractPageLoader;
 
-import java.io.IOException;
-import java.util.Objects;
-
-public class PageLoader implements PageLoaderInterface {
-
-    public final String STYLESHEET_PATH = getStyleSheet();
-    private Pane rootLayout;
-    private Stage stage;
-
-    @Override
-    public void setRootLayout(Pane rootLayout) {
-        this.rootLayout = rootLayout;
-    }
-
-    @Override
-    public void setStage(Stage stage) {
-        this.stage = stage;
-    }
-
-    @Override
-    public void loadPage(String fxmlFile, String title) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlFile));
-            rootLayout = fxmlLoader.load();
-            Scene scene = new Scene(rootLayout);
-            scene.getStylesheets().add(Objects.requireNonNull(Main.class.getResource(STYLESHEET_PATH)).toExternalForm());
-            stage.setScene(scene);
-            stage.setTitle(title);
-            stage.show();
-            stage.setResizable(false);
-            stage.getIcons().add(new Image("/logo.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+public class PageLoader extends AbstractPageLoader {
 
     @Override
     public void loadFirstPage() {
         loadLoginPage();
     }
 
+    @Override
     public void loadLoginPage() {
         loadPage("Login.fxml", "Solide™ Query Distributor - Login");
     }
 
+    @Override
     public void loadMainPage() {
         loadPage("Main.fxml", "Solide™ Query Distributor - Main");
     }
 
+    @Override
     public void loadAccountPage() {
         loadPage("Account.fxml", "Solide™ Query Distributor - Account");
     }
