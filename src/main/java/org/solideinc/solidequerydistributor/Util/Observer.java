8fc43d0d47e33fe5d484fd44adb7fb3cb662bdc7
SK™ <sohaib1411@hotmail.com>
2024-06-11 16:32:25 +0200

    Added tooltip to the Textfield observer

--- src/main/java/org/solideinc/solidequerydistributor/Util/Observer.java
+++ src/main/java/org/solideinc/solidequerydistributor/Util/Observer.java
@@ -3,6 +3,7 @@ package org.solideinc.solidequerydistributor.Util;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 
 public class Observer implements ChangeListener<String> {
     private final TextField textField;
@@ -26,9 +27,12 @@ public class Observer implements ChangeListener<String> {
         if (text.isEmpty()) {
             if (!textField.getStyleClass().contains(TEXT_FIELD_ERROR_CSS_CLASS)) {
                 textField.getStyleClass().add(TEXT_FIELD_ERROR_CSS_CLASS);
+                Tooltip tooltip = new Tooltip("Dit veld kan niet leeg zijn");
+                Tooltip.install(textField, tooltip);
             }
         } else {
             textField.getStyleClass().remove(TEXT_FIELD_ERROR_CSS_CLASS);
+            Tooltip.uninstall(textField, textField.getTooltip());
         }
     }
 }
